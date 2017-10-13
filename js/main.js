c4a7e6c9f0f18c65866f08a9eadca6dde617ab95
Treyhboy <taruntrehen@gmail.com>
2017-10-14 03:22:53 +0530

    full front end after submit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-var flag =1;
 window.onload = function () {
+    let over = document.getElementById('overview');
+    let trans = document.getElementById('trans');
+    let bills = document.getElementById('bills');
+    let spend = document.getElementById('spend');
+    let incest = document.getElementById('invest');
 
+    over.click(function () {
+        over();
+    })
+
+
+};
+function over()
+{
+$('box').empty();
+$('box').append(``)
 }
