fc441909d28fa282e78757445d93b1a158e62f2f
Pratihar <Gagandeep.Pratihar@cbre.com>
2018-10-03 12:35:18 +0530

    Need to Perform Testing with mysql

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const Investment = require('./db').investment;
 const spending = require('./db').spendings;
 const reminder = require('./db').reminder;
 const passport = require('passport');
+const  session = require('express-session')
+
+require('./Public_static/js/passport.js')(passport, user);
 
 app.use('/', express.static(__dirname + "/Public_static"))
 app.use(bp.urlencoded({extended: true}))
@@ -18,35 +21,54 @@ app.use(passport.initialize());
 
 app.use(passport.session()); // persistent login sessions
 
-app.post('/signup',(req,res) => {
-    user.create({
-        name:req.body.fname+req.body.lname,
-        mob:req.body.mob,
-        username:req.body.em,
-        pass:req.body.pass
-        }).then(function () {
-            res.send({success:true})
-        }).catch(function(err)
-        {
-            throw err;
-        });
-});
-app.post('/login',(req,res)=> {
-    user.findAll({where: {username: req.body.user, pass: req.body.pass}}).then(
-        function (db) {
-            if(db[0]) {
-                console.log('found')
-                res.send({status: 'found',name:db[0].name})
-            }else {
-                console.log('not');
-                res.send({status: 'not found'})
+// app.post('/signup',(req,res) => {
+//     user.create({
+//         name:req.body.fname+req.body.lname,
+//         mob:req.body.mob,
+//         username:req.body.em,
+//         pass:req.body.pass
+//         }).then(function () {
+//             res.send({success:true})
+//         }).catch(function(err)
+//         {
+//             throw err;
+//         });
+// });
 
-            }
-        }).catch(function (err) {
-        console.log('err');
-            res.send(err)
-    })
-});
+ app.post('/signup', passport.authenticate('local-signup', {
+            successRedirect: '/',
+            failureRedirect: '/signup',
+            failureFlash: true
+        }
+ 
+    ));
+
+
+// app.post('/login',(req,res)=> {
+//     user.findAll({where: {username: req.body.user, pass: req.body.pass}}).then(
+//         function (db) {
+//             if(db[0]) {
+//                 console.log('found')
+//                 res.send({status: 'found',name:db[0].name})
+//             }else {
+//                 console.log('not');
+//                 res.send({status: 'not found'})
+
+//             }
+//         }).catch(function (err) {
+//         console.log('err');
+//             res.send(err)
+//     })
+// });
+
+app.post('/login', passport.authenticate('local-signin', {
+        successRedirect: {console.log('login success')},
+        failureRedirect: '/login',
+        failureFlash: true
+    }
+ 
+));
+
 app.post('/spen',(req,res)=> {
     spending.findAll({where: {user: req.body.user}}).then(
         function (db) {
@@ -61,6 +83,7 @@ app.post('/spen',(req,res)=> {
         res.send(err)
     })
 });
+
 app.post('/down',(req,res)=> {
     reminder.findAll({where: {user: req.body.user}}).then(
         function (db) {
@@ -75,6 +98,8 @@ app.post('/down',(req,res)=> {
         res.send(err)
     })
 });
+
+
 app.post('/invest',(req,res)=> {
     Investment.findAll({where: {user: req.body.user}}).then(
         function (db) {
