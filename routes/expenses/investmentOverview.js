c6cf0232cfa7e7a94348d74603a5a0fe695d81ae
Aaditya Chakravarty <ac36500@gmail.com>
2019-10-06 21:26:07 +0530

    Modularized entire Backend and fixed npm issues.

---
+++ routes/expenses/investmentOverview.js
@@ -0,0 +1,17 @@
+const Investment = require('../../db').investment;
+
+const getOverview = async (req, res) => {
+    try {
+        await Investment.findAll({
+            where: {
+                user: req.body.user
+            }
+        });
+        res.send(result);
+    } catch (err) {
+        console.log("err");
+        res.send(err);
+    }
+}
+
+module.exports = getOverview;
