b8446eaca0251f037f1bc00843738f0fe5a34727
eglove <glover.ethan@gmail.com>
2020-04-15 10:58:03 -0500

    Setting up ESLint, these are the rules I typically use, let me know if these are acceptable and I'll continue. Also changed long ternary operator in signup.js to if statement for readability.

--- Public_static/js/plugins.js
+++ Public_static/js/plugins.js
@@ -1,15 +1,36 @@
 // Avoid `console` errors in browsers that lack a console.
 (function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    let method;
+    const noop = function () {
+    };
+    const methods = [
+        'assert',
+        'clear',
+        'count',
+        'debug',
+        'dir',
+        'dirxml',
+        'error',
+        'exception',
+        'group',
+        'groupCollapsed',
+        'groupEnd',
+        'info',
+        'log',
+        'markTimeline',
+        'profile',
+        'profileEnd',
+        'table',
+        'time',
+        'timeEnd',
+        'timeline',
+        'timelineEnd',
+        'timeStamp',
+        'trace',
+        'warn',
     ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
+    let {length} = methods;
+    const console = (window.console = window.console || {});
 
     while (length--) {
         method = methods[length];
@@ -19,6 +40,6 @@
             console[method] = noop;
         }
     }
-}());
+})();
 
 // Place any jQuery/helper plugins in here.
--- Public_static/js/signup.js
+++ Public_static/js/signup.js
@@ -1,41 +1,42 @@
 $(function() {
-  let fname = $("#fname");
-  let lname = $("#lname");
-  let mob = $("#mob");
-  let em = $("#em");
-  let pass = $("#pass");
-  let submit = $("#create-account");
-  submit.click(function() {
-    $.post(
-      "/signup",
-      {
-        firstname: fname.val(),
-        lastname: lname.val(),
-        mobile: mob.val(),
-        email: em.val(),
-        password: pass.val()
-      },
-      function(data) {
-        if (data.success) {
-          window.location.href = "http://localhost:3100/";
-          sessionStorage.setItem("name", fname.val() + lname.val());
+    const fname = $('#fname');
+    const lname = $('#lname');
+    const mob = $('#mob');
+    const em = $('#em');
+    const pass = $('#pass');
+    const submit = $('#create-account');
+    submit.click(function () {
+        $.post(
+            '/signup',
+            {
+                firstname: fname.val(),
+                lastname: lname.val(),
+                mobile: mob.val(),
+                email: em.val(),
+                password: pass.val(),
+            },
+            function (data) {
+                if (data.success) {
+                    window.location.href = 'http://localhost:3100/';
+                    sessionStorage.setItem('name', fname.val() + lname.val());
+                }
+            }
+        );
+    });
+    $('input').on('focus', function (e) {
+        $(e.currentTarget)
+            .siblings('label')
+            .addClass('active');
+    });
+    $('input').on('focusout', function (e) {
+        if ($(e.currentTarget).prop('value').length > 0) {
+            $(e.currentTarget)
+                .siblings('label')
+                .addClass('active');
         } else {
+            $(e.currentTarget)
+                .siblings('label')
+                .removeClass('active');
         }
-      }
-    );
-  });
-  $("input").on("focus", function(e) {
-    $(e.currentTarget)
-      .siblings("label")
-      .addClass("active");
-  });
-  $("input").on("focusout", function(e) {
-    $(e.currentTarget).prop("value").length > 0
-      ? $(e.currentTarget)
-          .siblings("label")
-          .addClass("active")
-      : $(e.currentTarget)
-          .siblings("label")
-          .removeClass("active");
-  });
+    });
 });
