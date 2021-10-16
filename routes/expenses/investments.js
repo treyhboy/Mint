fcf8477c2b2fb8e24f951e725e8e99e17d5f5106
Pizzo15 <lpizzini7@gmail.com>
2021-10-16 17:09:06 +0200

    fix: adjusted model name and query

--- routes/expenses/investments.js
+++ routes/expenses/investments.js
@@ -1,15 +1,15 @@
-const Investment = require('../../db').investment;
+const { Investment } = require("../../db");
 
 const getInvestments = async (req, res) => {
     try {
-        const db = await Investment.findAll({ where: { user: req.body.user } });
-        if (db[0]) {
-            res.send({ status: 'found', data: db });
-        } else {
-            res.send({ status: 'not found' });
+        const db = await Investment.findAll({ user: req.body.user });
+        if (db.length === 0) {
+            return res.send({ status: "not found" });
         }
+
+        res.send({ status: "found", data: db });
     } catch (err) {
-        console.log('err');
+        console.log("err");
         res.send(err);
     }
 };
