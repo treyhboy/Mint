d7b46d5ee0d7f08660376340cc6795c2c502bc71
Pizzo15 <lpizzini7@gmail.com>
2021-10-16 17:06:05 +0200

    fix: moved passport-google-oauth import

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-require('dotenv').config({ path: `${process.cwd()}/.env.example` });
+require("dotenv").config({ path: `${process.cwd()}/.env.example` });
 
 /**
  * @description database tables using Sequelize
@@ -11,111 +11,111 @@ const db = new Sequelize({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.PORT,
-    dialect: 'mysql',
+    dialect: "mysql"
 });
 
 /**
  * @description table name: credit , elements:mode, detail, amount
  */
-const credit = db.define('credit', {
+const Credit = db.define("credit", {
     id: {
         type: Sequelize.DataTypes.INTEGER,
         primaryKey: true,
-        autoIncrement: true,
+        autoIncrement: true
     },
     mode: Sequelize.DataTypes.STRING,
     detail: Sequelize.DataTypes.BOOLEAN,
-    amount: Sequelize.DataTypes.INTEGER,
+    amount: Sequelize.DataTypes.INTEGER
 });
 
 /**
  * @description table name: user , elements:username, email, pass, name, mob, budget
  */
-const user = db.define('user', {
+const User = db.define("user", {
     id: {
         type: Sequelize.DataTypes.INTEGER,
         primaryKey: true,
-        autoIncrement: true,
+        autoIncrement: true
     },
     username: Sequelize.DataTypes.STRING,
     email: Sequelize.DataTypes.STRING,
     password: Sequelize.DataTypes.STRING,
     name: Sequelize.DataTypes.STRING,
     mobile: Sequelize.DataTypes.STRING,
-    Budget: Sequelize.DataTypes.INTEGER,
+    Budget: Sequelize.DataTypes.INTEGER
 });
 
 /**
  * @description table name: spendings , elements:user, detail, amount, Mode
  */
-const spendings = db.define('spendings', {
+const Spendings = db.define("spendings", {
     id: {
         type: Sequelize.DataTypes.INTEGER,
         primaryKey: true,
-        autoIncrement: true,
+        autoIncrement: true
     },
     user: Sequelize.DataTypes.STRING,
     detail: Sequelize.DataTypes.STRING,
     amount: Sequelize.DataTypes.INTEGER,
-    Mode: Sequelize.DataTypes.STRING,
+    Mode: Sequelize.DataTypes.STRING
 });
 
 /**
  * @description  table name: investment , elements:user, detail, amount, Mode
  */
-const investment = db.define('investment', {
+const Investment = db.define("investment", {
     id: {
         type: Sequelize.DataTypes.INTEGER,
         primaryKey: true,
-        autoIncrement: true,
+        autoIncrement: true
     },
     user: Sequelize.DataTypes.STRING,
     detail: Sequelize.DataTypes.STRING,
     amount: Sequelize.DataTypes.INTEGER,
-    Mode: Sequelize.DataTypes.STRING,
+    Mode: Sequelize.DataTypes.STRING
 });
 
 /**
  * @description  table name: reminder , elements:user, detail, amount, date
  */
-const reminder = db.define('reminders', {
+const Reminder = db.define("reminders", {
     id: {
         type: Sequelize.DataTypes.INTEGER,
         primaryKey: true,
-        autoIncrement: true,
+        autoIncrement: true
     },
     user: Sequelize.DataTypes.STRING,
     detail: Sequelize.DataTypes.STRING,
     amount: Sequelize.DataTypes.INTEGER,
-    date: Sequelize.DataTypes.STRING,
+    date: Sequelize.DataTypes.STRING
 });
 
 /**
  * @description  table name: transaction , elements:user, detail, amount, date, type
  */
-const transaction = db.define('transaction', {
+const Transaction = db.define("transaction", {
     id: {
         type: Sequelize.DataTypes.INTEGER,
         primaryKey: true,
-        autoIncrement: true,
+        autoIncrement: true
     },
     user: Sequelize.DataTypes.STRING,
     detail: Sequelize.DataTypes.STRING,
     amount: Sequelize.DataTypes.INTEGER,
     date: Sequelize.DataTypes.STRING,
     type: Sequelize.DataTypes.STRING,
-    Mode: Sequelize.DataTypes.STRING,
+    Mode: Sequelize.DataTypes.STRING
 });
 
 db.sync().then(function() {
-    console.log('Database is ready');
+    console.log("Database is ready");
 });
 
 module.exports = {
-    user,
-    spendings,
-    credit,
-    reminder,
-    investment,
-    transaction,
+    User,
+    Spendings,
+    Credit,
+    Reminder,
+    Investment,
+    Transaction
 };
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 /**
  * @summary server use passport express mysql sequelize socket.io
  */
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const passport = require('passport');
-const session = require('express-session');
-const response = require('./utils-module/response');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const session = require("express-session");
+const response = require("./utils-module/response");
+const { User } = require("./db");
 
 const PORT = 3100;
 
@@ -15,52 +16,55 @@ const PORT = 3100;
 //   Strategies in Passport require a `verify` function, which accept
 //   credentials (in this case, an accessToken, refreshToken, and Google
 //   profile), and invoke a callback with a user object.
-passport.use(new GoogleStrategy({
-    clientID: 'CLIENT_ID',
-    clientSecret: 'CLIENT_SECRET',
-    callbackURL: `http://localhost:${PORT}/google/callback`
-},
-    async function (accessToken, refreshToken, profile, done) {
-        try {
-            const { email, emails, displayName } = profile
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: "CLIENT_ID",
+            clientSecret: "CLIENT_SECRET",
+            callbackURL: `http://localhost:${PORT}/google/callback`
+        },
+        async function(accessToken, refreshToken, profile, done) {
+            try {
+                const { email, emails, displayName } = profile;
 
-            const user = await user.findOne({ 'email': email })
+                const existingUser = await User.findOne({ email });
 
-            if (user) {
-                // User found
-                return done(null, user);
+                if (existingUser) {
+                    // User found
+                    return done(null, existingUser);
+                }
+
+                // Creating a new user if not exist
+                const newUser = await User.create({
+                    username: displayName,
+                    email: emails[0].value
+                });
+
+                return done(null, newUser);
+            } catch (err) {
+                return done(err, null);
             }
-
-            // Creating a new user if not exist
-            const newUser = await user.create({
-                username: displayName,
-                email: emails[0].value,
-            })
-
-            return done(null, newUser)
-        } catch (err) {
-            return done(err, null);
         }
-    }
-));
+    )
+);
 
 // To locate the config folder by default it searches for .env here it is .env.example
 
 // Sample ENV properties are loaded here in reallife scenario this would be populated by the environment property of machine running the app server
 
-require('dotenv').config({ path: `${process.cwd()}/.env.example` });
+require("dotenv").config({ path: `${process.cwd()}/.env.example` });
 
-app.use('/', express.static(`${__dirname}/Public_static`));
+app.use("/", express.static(`${__dirname}/Public_static`));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
-    session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })
+    session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
 ); // session secret
 
 // Passport Authentication Implementation
 
-require('./config/passport.js');
+require("./config/passport.js");
 
 app.use(passport.initialize());
 
@@ -68,8 +72,8 @@ app.use(passport.session()); // persistent login sessions
 
 // Sign up Implementation
 
-app.post('/signup', (req, res, next) => {
-    passport.authenticate('local-signup', function (err, user, info) {
+app.post("/signup", (req, res, next) => {
+    passport.authenticate("local-signup", function(err, user, info) {
         if (err) {
             return response.responseWriter(res, 500, info);
         }
@@ -83,8 +87,8 @@ app.post('/signup', (req, res, next) => {
 
 // Login value is sent to
 
-app.post('/login', (req, res, next) => {
-    passport.authenticate('local-signin', function (err, user, info) {
+app.post("/login", (req, res, next) => {
+    passport.authenticate("local-signin", function(err, user, info) {
         if (err) {
             return response.responseWriter(res, 500, info);
         }
@@ -101,8 +105,12 @@ app.post('/login', (req, res, next) => {
 //   request.  The first step in Google authentication will involve
 //   redirecting the user to google.com.  After authorization, Google
 //   will redirect the user back to this application at /auth/google/callback
-app.get('/google', (req, res, next) => {
-    passport.authenticate('google', { scope: ['profile', 'email'] })(req, res, next);
+app.get("/google", (req, res, next) => {
+    passport.authenticate("google", { scope: ["profile", "email"] })(
+        req,
+        res,
+        next
+    );
 });
 
 // GET /auth/google/callback
@@ -110,16 +118,22 @@ app.get('/google', (req, res, next) => {
 //   request.  If authentication fails, the user will be redirected back to the
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
-app.get('/google/callback', (req, res, next) => {
-    passport.authenticate('google', { failureRedirect: '/login' })(req, res, next)
-},
-    function (req, res) {
-
+app.get(
+    "/google/callback",
+    (req, res, next) => {
+        passport.authenticate("google", { failureRedirect: "/login" })(
+            req,
+            res,
+            next
+        );
+    },
+    (req, res) => {
         // Redirecting to /main URL after successful login
         res.redirect(`http://localhost:${PORT}/main.html`);
-    });
+    }
+);
 
-app.use('/', require('./routes/index'));
+app.use("/", require("./routes/index"));
 
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
