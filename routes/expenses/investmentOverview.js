e967c31a5914d2c180e2bb1b00bc32d6cd9c8c8a
Pizzo15 <lpizzini7@gmail.com>
2021-10-16 17:07:41 +0200

    fix: adjusted model name

--- routes/expenses/investmentOverview.js
+++ routes/expenses/investmentOverview.js
@@ -1,15 +1,15 @@
-const Investment = require('../../db').investment;
+const { Investment } = require("../../db");
 
 const getOverview = async (req, res) => {
     try {
         const result = await Investment.findAll({
             where: {
-                user: req.body.user,
-            },
+                user: req.body.user
+            }
         });
         res.send(result);
     } catch (err) {
-        console.log('err');
+        console.log("err");
         res.send(err);
     }
 };
